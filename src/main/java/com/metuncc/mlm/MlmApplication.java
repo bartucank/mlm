36094d0131f58d608852a5dfd4c5a9ad2d7ddc13
bartucank <bartucankk@gmail.com>
2023-10-19 15:35:44 +0300

    Hello World!

---
+++ src/main/java/com/metuncc/mlm/MlmApplication.java
@@ -0,0 +1,13 @@
+package com.metuncc.mlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metuncc/mlm/MlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metuncc.mlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
